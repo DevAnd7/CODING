94a79461ab20575d4f5356b7654402136616c475
Andri Arrieta <andriarrieta7@gmail.com>
2026-03-23 11:45:46 +0000

    bootstrap

--- practicando-react/src/App.jsx
+++ practicando-react/src/App.jsx
@@ -6,8 +6,11 @@ const App = () => {
       <h1>
         Practicando React
       </h1>
+
+      <div className="d-flex gap-2">
       <Boton letra= 'A' mensaje='Oprimiste el Boton A'/>
       <Boton letra= 'B' mensaje='Oprimiste el Boton B'/>
+      </div>
     </div>
   );
 };
